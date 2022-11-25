641587db88aa69a57b8ea1832c5fcde8a42bc5e9
gusfelix <109548569+gusfelix@users.noreply.github.com>
2022-11-25 10:03:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ boxes.forEach(box =>{
     box.classList.add('highlight')
 })
 
+
 if(localStorage.getItem('game-data')){
-
+    
     let data = JSON.parse(localStorage.getItem('game-data'));
-
+    
     data.row = 1;
     data.won = false;
     data.word = [];
-
+    
     localStorage.setItem('game-data', JSON.stringify(data))
-
+    
     for(let w of data.attempts){       
-
+        
         for(let l of w){
             addLetter(l);
         }
@@ -25,9 +26,11 @@ if(localStorage.getItem('game-data')){
         
         let data = JSON.parse(localStorage.getItem('game-data'));
         data.attempts.pop();
-
+        
         localStorage.setItem('game-data', JSON.stringify(data))
     }
+    
+    graphic();
 
 }else{
     openModal('#howToPlay');
@@ -37,11 +40,13 @@ if(localStorage.getItem('game-data')){
         word : [],
         attempts : [],
         solution : setWord(),
-        solutionLetters : {a:0, b:0, c:0, d:0, e:0, f:0, g:0, h:0, i:0, j:0, k:0, l:0, m:0, n:0, o:0, p:0, q:0, r:0, s:0, t:0, u:0, v:0, w:0, x:0, y:0, z:0, },
+        solutionLetters : {},
         wins : 0,
         losses : 0,
+        winDistribution : [0, 0, 0, 0, 0, 0],
         winStreak : 0,
-        matches : 1,
+        maxStreak : 0 ,
+        matches : 0,
         won : false
     }
 
@@ -102,10 +107,10 @@ function delLetter(){
 function validWord(){
     
     let data = JSON.parse(localStorage.getItem('game-data'));
-    data.attempts.push(data.word);
     
-    if(data.word.length == 5 && wordIn(data.word.join(''))){ //verifica se o tamanha da palavra é 5 e se ela é válida
-
+    if(data.word.length == 5 && wordIn(data.word.join(''))) { //verifica se o tamanha da palavra é 5 e se ela é válida
+        
+        data.attempts.push(data.word);
         const solution = data.solution.split('');
         
         data.word.forEach((letter, index) =>{ ; //verifica e coloca as cores
@@ -117,10 +122,6 @@ function validWord(){
                 document.getElementById(`${data.row}-${index + 1}`).classList.add('right');
                 document.getElementById(`key-${letter}`).classList.add('right');
                 document.getElementById(`key-${letter}`).classList.remove('wrong-place');
-
-                if(indexOf > -1){
-                    solution.splice(indexOf, 1, '');
-                }
                 
                 boxes.forEach(box =>{
                     if(box.getAttribute("value") == letter && box.classList.contains('wrong-place') && contLetters(letter, data.word) > data.solutionLetters[letter]){
@@ -133,26 +134,25 @@ function validWord(){
                 document.getElementById(`${data.row}-${index + 1}`).classList.add('wrong-place');
                 document.getElementById(`key-${letter}`).classList.add('wrong-place');     
                 
-                if(indexOf > -1){
-                    solution.splice(indexOf, 1, '');
-                }
-                
             }else if(!data.solution.includes(letter)){
                 document.getElementById(`key-${letter}`).classList.add('wrong');
             }
+
+            if(indexOf > -1) solution.splice(indexOf, 1, '');
+            
         })
         
         if(data.word.join('') == data.solution){ //case win
             
             data.won = true;
-            document.getElementById('result-message').innerHTML = 'Parabéns, você acertou!';
+            document.getElementById('result-message').innerHTML = 'Parabéns, você acertou! :)';
 
             boxes.forEach(box =>{
                 box.classList.remove('highlight');
                 box.classList.remove('invalid');
             })
             
-            setTimeout(() => openModal('#result'), 1000)
+            setTimeout(() => openResultModal(), 1000)
             
         }else if(data.row >= 6){ //case lost
             
@@ -164,10 +164,10 @@ function validWord(){
                 box.classList.remove('invalid');
             })
 
-            setTimeout(() => openModal('#result'), 1000);
+            setTimeout(() => openResultModal(), 1000);
 
         }else{
-            data.row  += 1;
+            data.row++;
             col = 1;
             data.word = [];
             
@@ -195,8 +195,8 @@ function setWord(){
 
     if(regexWord.test(word) || word.length != 5){
         setWord();
-    }else{
 
+    }else{
         return word;
     }
 }
@@ -216,14 +216,17 @@ function wordIn(word){
 }
 
 function contLettersSolution(){
-
     let data = JSON.parse(localStorage.getItem('game-data'));
-
-    for(letter of data.solution){
-        data.solutionLetters[letter]++
+    
+    for(let letter of data.solution) {
+        data.solutionLetters[letter] = 0;
     }
 
-    localStorage.setItem('game-data', JSON.stringify(data))
+    for(letter of data.solution){
+        data.solutionLetters[letter]++;
+    }
+
+    localStorage.setItem('game-data', JSON.stringify(data));
 }
 
 function contLetters(letter, array){
@@ -245,6 +248,10 @@ function restartGame(){
     if(data.won == true){
         data.wins++;
         data.winStreak++;
+        if(data.winStreak > data.maxStreak){
+            data.maxStreak = data.winStreak;
+        }
+        data.winDistribution[data.row - 1]++;
     }else{
         data.losses++;
         data.winStreak = 0;
@@ -255,14 +262,35 @@ function restartGame(){
     data.solution = setWord();
     data.won = false;
     data.matches++;
-    Object.keys(data.solutionLetters).forEach(key =>{data.solutionLetters[key] = 0});
+    // Object.keys(data.solutionLetters).forEach(key =>{data.solutionLetters[key] = 0});
+    data.solutionLetters = {};
 
-    localStorage.setItem('game-data', JSON.stringify(data))
+    localStorage.setItem('game-data', JSON.stringify(data));
 
     contLettersSolution();
     location.reload();
 }
 
 function openModal(id){
-    $(id).modal({show: true})
+    $(id).modal({show: true});
+}
+
+function openResultModal(){
+    $('#result').modal({show: true, backdrop: 'static', keyboard: false})  
+    
+}
+
+function graphic(){
+
+    const gameBoard = document.getElementById('graphic');
+    let data = JSON.parse(localStorage.getItem('game-data'));
+    
+    for(let i = 0; i < 5; i++){
+        let bar = document.createElement('div');
+
+        bar.classList.add('graphic-bar');
+        bar.style.width = `${(data.winDistribution[i]/data.wins) * 100}%`;
+
+        gameBoard.appendChild(bar);
+    }
 }
